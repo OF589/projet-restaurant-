cd6286a2a8b414f925623bdca2693eede0b644a2
OF589 <oumniafarit@gmail.com>
2026-01-03 20:18:47 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,18 +1,11 @@
-// Fonction de connexion
-function login() {
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
-  const error = document.getElementById("error");
+document.getElementById('loginBtn').addEventListener('click', ()=>{
+    const username = document.getElementById('username').value.trim();
+    const password = document.getElementById('password').value.trim();
+    const errorMsg = document.getElementById('errorMsg');
 
-  if (username === "admin" && password === "admin") {
-    // Redirection vers le dashboard
-    window.location.href = "dashboard.html";
-  } else {
-    error.textContent = "Identifiant ou mot de passe incorrect";
-  }
-}
-
-// Fonction de déconnexion
-function logout() {
-  window.location.href = "index.html";
-}
+    if(username === 'admin' && password === 'admin'){
+        window.location.href = 'dashboard.html';
+    } else {
+        errorMsg.textContent = "Nom d'utilisateur ou mot de passe incorrect";
+    }
+});// Fonction de connexion
